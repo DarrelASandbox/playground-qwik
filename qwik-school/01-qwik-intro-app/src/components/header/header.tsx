fbf767de6e8ee483614cbfb058c9deb4b413abee
DarrelA <darrelaiscoding@gmail.com>
2023-05-14 16:01:47 +0800

    Replaced `components` & `routes` folders and `global.css`

---
+++ qwik-school/01-qwik-intro-app/src/components/header/header.tsx
@@ -0,0 +1,33 @@
+import { component$, useStylesScoped$ } from '@builder.io/qwik';
+import styles from './header.css?inline';
+
+export default component$(() => {
+  useStylesScoped$(styles);
+
+  return (
+    <header>
+      <div class="logo">
+        
+          <h3><a href="/">HEADER LOGO</a></h3>
+        
+      </div>
+      <ul>
+        <li>
+          <a href="/page1" >
+            Page 1
+          </a>
+        </li>
+        <li>
+          <a href="/page2">
+            Page 2
+          </a>
+        </li>
+        <li>
+          <a href="/page3">
+            Page 3
+          </a>
+        </li>
+      </ul>
+    </header>
+  );
+});
--- qwik-school/01-qwik-intro-app/src/routes/layout.tsx
+++ qwik-school/01-qwik-intro-app/src/routes/layout.tsx
@@ -1,26 +1,24 @@
-import { component$, Slot, useStyles$ } from '@builder.io/qwik';
-import { routeLoader$ } from '@builder.io/qwik-city';
-
-import Header from '~/components/starter/header/header';
-import Footer from '~/components/starter/footer/footer';
-
-import styles from './styles.css?inline';
-
-export const useServerTimeLoader = routeLoader$(() => {
-  return {
-    date: new Date().toISOString(),
-  };
-});
+import { component$, Slot } from '@builder.io/qwik';
+import Header from '../components/header/header';
+import Footer from '../components/footer/footer';
 
 export default component$(() => {
-  useStyles$(styles);
+  
   return (
-    <>
+    <div class="layout">
+      
+      I AM THE LAYOUT
+
       <Header />
+
       <main>
-        <Slot />
+        <section class="page">
+          <Slot />
+        </section>
       </main>
+
       <Footer />
-    </>
+      
+    </div>
   );
 });
