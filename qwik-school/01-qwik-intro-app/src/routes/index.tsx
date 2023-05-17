8f079be124891ccfc5685a7cd335f86ed50ca604
DarrelA <darrelaiscoding@gmail.com>
2023-05-17 22:23:10 +0800

    `useTask` hook

--- qwik-school/01-qwik-intro-app/src/routes/index.tsx
+++ qwik-school/01-qwik-intro-app/src/routes/index.tsx
@@ -1,23 +1,39 @@
-import { Slot, component$, useSignal } from '@builder.io/qwik';
+import type { Signal } from '@builder.io/qwik';
+import { Slot, component$, useSignal, useTask$ } from '@builder.io/qwik';
 
 export default component$(() => {
   const isMiskoVisibleSignal = useSignal(false);
+  const didHeGetABeerSignal = useSignal(false);
+
+  useTask$(({ track }) => {
+    // Don't execute the code unless the value changes
+    track(() => didHeGetABeerSignal.value);
+    if (didHeGetABeerSignal.value) isMiskoVisibleSignal.value = true;
+  });
 
   return (
     <>
-      <div>Llamas</div>
-      <button
-        onClick$={() => {
-          isMiskoVisibleSignal.value = !isMiskoVisibleSignal.value;
-          console.log('Ahoj!');
-        }}
-      >
+      <button onClick$={() => (isMiskoVisibleSignal.value = !isMiskoVisibleSignal.value)}>
         Ahoj!
       </button>
+
+      <BeerGiver gotBeerSignal={didHeGetABeerSignal} />
       {isMiskoVisibleSignal.value && <Misko>This is a slot.</Misko>}
     </>
   );
 });
+
+interface BeerGiverProps {
+  gotBeerSignal: Signal<boolean>;
+}
+
+const BeerGiver = component$((props: BeerGiverProps) => (
+  <div>
+    <button onClick$={() => (props.gotBeerSignal.value = true)}>
+      Give a beer to Misko
+    </button>
+  </div>
+));
 
 const Misko = component$(() => (
   <div>
